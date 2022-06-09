6f7827c9041779c451e59f04c2fb2f2a75a1ddd1
jpcmorais16 <jpcmorais2001@gmail.com>
2022-06-09 15:36:22 -0300

    alteracoes na tela

--- desktop/src/com/plantsvszombies/game/DesktopLauncher.java
+++ desktop/src/com/plantsvszombies/game/DesktopLauncher.java
@@ -10,6 +10,8 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("Plants Vs Zombies");
+		config.setResizable(false);
+		config.setWindowedMode(1365, 700);
 		new Lwjgl3Application(new PlantsVsZombies(), config);
 	}
 }
